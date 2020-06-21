01a45e5b3fa3f7e7f283a931d816f4fc8489f282
hubert <hubert.leleu@epitech.eu>
2020-06-21 10:26:19 +0200

    code optimization + more error messages

--- scraproxy.py
+++ scraproxy.py
@@ -6,14 +6,17 @@ import requests
 from fake_useragent import UserAgent
 import itertools as it
 import cloudscraper
+import csv
 
-def get_pages(token, nb, jump):
+def get_pages(token):
     pages = []
-    for i in range(1, nb+1, jump):
-        if nb != 1:
-            j = token + str(i)
+    for i in range(1, token[1]+1, token[2]):
+        if token[1] != 1 and token[3] == 0:
+            j = token[0] + str(i)
+        elif token[1] != 1 and token[3] == 1:
+            j = token[0] + "0" + str(i) + ".htm"
         else:
-            j = token
+            j = token[0]
         pages.append(j)
     return pages
 
@@ -23,7 +26,14 @@ def parseLine(line):
     for balise in line:
         for content in balise:
             stock.append(content)
-    print(stock)
+    print("- step n°3 done [", len(stock), "]")
+
+def connector(i, proxy, ua):
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(i, proxies={"http": proxy, "https": proxy}, headers={'User-Agent': ua.random}, timeout=5)
+    time.sleep(random.randrange(1,4))
+    print("- connection done")
+    return bs4.BeautifulSoup(response.text, 'html.parser')
 
 def get_data(pages,proxies):
     df = pd.DataFrame()
@@ -37,36 +47,35 @@ def get_data(pages,proxies):
             df_f = pd.DataFrame()
             if lock < 1:
                 proxy = next(proxy_pool)
-                print("\n( ", proxy, " ) - ", end=" ")
+                print("\n( ", proxy, " ) :")
             try:
-                scraper = cloudscraper.create_scraper()
-                response = scraper.get(i, proxies={"http": proxy, "https": proxy}, headers={'User-Agent': ua.random}, timeout=5)
-                time.sleep(random.randrange(1,4))
+                soup = connector(i, proxy, ua)
                 lock = 1
-                soup = bs4.BeautifulSoup(response.text, 'html.parser')
                 tr_box = soup.find_all("tr")
+                print("- step n°2 done [", len(tr_box), "]")
+
                 for l in tr_box:
                     parseLine(l)
-
-                print("-> ", len(pages), " pages left to analyze.)")
+                print(len(pages), " pages left to analyze.")
                 pages.remove(i)
-            except:
+
+            except requests.Timeout as err:
                 lock = 0
-                print(" - fail")
+            except:
+                print(err.message)
 
     return df
 
-def scrapper(token, proxies, nbrpages, jump):
-    pages = get_pages(token, nbrpages, jump)
-    return get_data(pages,proxies)
-
 def launcher(proxies):
-    token1 = ["https://hidemy.name/en/proxy-list/?start=", 1280, 64] ###CLOUDFLARE PROTECTION###
-    token2 = ["https://www.ip-adress.com/proxy-list", 1, 1]
-    token3 = ["http://free-proxy.cz/en/proxylist/main/", 150, 1] ###CAPTCHAT###
-    tokens = [token1]
+    token0 = ["http://nntime.com/proxy-list-", 7, 1, 1]
+    token1 = ["https://hidemy.name/fr/proxy-list/?start=", 1280, 64, 0] ###CLOUDFLARE PROTECTION###
+    token2 = ["https://www.ip-adress.com/proxy-list", 1, 1, 0]
+    token3 = ["http://free-proxy.cz/en/proxylist/main/", 150, 1, 0] ###CAPTCHAT###
+
+    tokens = [token0]
     for tok in range(0, len(tokens)):
-        scrapper(tokens[tok][0], proxies, tokens[tok][1], tokens[tok][2])
+
+        get_data(get_pages(tokens[tok]),proxies)
 
 def main():
     proxies = pd.read_csv('Proxy_List.txt', header = None)
