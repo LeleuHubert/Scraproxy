dbb0c111f8b7eda89972f0341ebe5fe406d561c8
hubert <hubert.leleu@epitech.eu>
2020-06-22 10:14:44 +0200

    add www.socks-proxy.net

--- scraproxy.py
+++ scraproxy.py
@@ -1,28 +1,34 @@
+import requests, traceback
 from lxml.html import fromstring
-import requests
 from itertools import cycle
-import traceback
 
-def get_infoFrom(url):
+def get_infoFrom(url, key):
     response = requests.get(url)
     parser = fromstring(response.text)
     proxies = set()
-    for i in parser.xpath('//tbody/tr')[:100]:
-        if i.xpath('.//td[7][contains(text(),"yes")]'):
+    for i in parser.xpath('//tbody/tr')[:1000]:
+        if i.xpath('.//td[7][contains(text(),"yes")]') and key == 0:
             proxy = ";".join(["https",i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
             proxies.add(proxy)
-        elif i.xpath('.//td[7][contains(text(),"no")]'):
+        elif i.xpath('.//td[7][contains(text(),"no")]') and key == 0:
             proxy = ";".join(["http",i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+        elif i.xpath('.//td[7][contains(text(),"Yes")]') and key == 1:
+            proxy = ";".join([i.xpath('.//td[5]/text()')[0],i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
             proxies.add(proxy)
     return proxies
 
+def launcher():
+    with open('list.csv', 'a') as file:
+        for elem in get_infoFrom("https://free-proxy-list.net/", 0):
+            file.write('%s\n' % elem)
+        for elem in get_infoFrom("https://www.socks-proxy.net/", 1):
+            file.write('%s\n' % elem)
+
+
 
 def main():
-    info = get_infoFrom("https://free-proxy-list.net/")
-
-    with open('list.csv', 'a') as file:
-        for elem in info:
-            file.write('%s\n' % elem)
+    launcher()
 
 if __name__== "__main__":
   main()
