a0cfdd31cecc2607cd3e04c7c5dc6489b76ff230
hubert <hubert.leleu@epitech.eu>
2020-06-23 14:16:03 +0200

    add cleaner function

--- scraproxy.py
+++ scraproxy.py
@@ -1,4 +1,4 @@
-import requests, traceback, cloudscraper
+import requests, traceback, cloudscraper, re
 from lxml.html import fromstring
 from itertools import cycle
 
@@ -15,7 +15,7 @@ def next_page(url, info):
     return pages
 
 def get_info(parser, proxies, key):
-    for i in parser.xpath('//tbody/tr')[:30]:
+    for i in parser.xpath('//tbody/tr')[:300]:
         if i.xpath('.//td[7][contains(text(),"yes")]') and key == 0:
             proxy = ";".join(["https",i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
             proxies.add(proxy)
@@ -39,15 +39,34 @@ def get_infoFrom(url, key):
     proxies = get_info(parser, proxies, key)
     return proxies
 
+def cleaner(proxies):
+    proxies = [proxy.replace(';', ':') for proxy in proxies]
+    proxies = [proxy.replace('https:', '') for proxy in proxies]
+    proxies = [proxy.replace('http:', '') for proxy in proxies]
+
+    return proxies
+
+def tester(proxies):
+    print(proxies)
+
+
 def launcher():
+    proxiAdrss = []
+
+    print("- scrapping step starting")
     with open('list.csv', 'a') as file:
         for elem in get_infoFrom("https://free-proxy-list.net/", 0):
+            proxiAdrss.append(elem)
             file.write('%s\n' % elem)
         for elem in get_infoFrom("https://www.socks-proxy.net/", 1):
-              file.write('%s\n' % elem)
-        for page in next_page("https://hidemy.name/en/proxy-list/?start=", [1216,64,0]):
-            for elem in get_infoFrom(page, 2):
-                file.write('%s\n' % elem)
+            proxiAdrss.append(elem)
+            file.write('%s\n' % elem)
+        # for page in next_page("https://hidemy.name/en/proxy-list/?start=", [1216,64,0]):
+        #     for elem in get_infoFrom(page, 2):
+        #         proxiAdrss.append(elem)
+        #         file.write('%s\n' % elem)
+    print("- testing step starting")
+    tester(cleaner(proxiAdrss))
 
 def main():
     print("-> Scraproxy has started")
