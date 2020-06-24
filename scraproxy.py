0fceab34045eac5e39b634fe12e64052aa12f57c
hubert <hubert.leleu@epitech.eu>
2020-06-24 16:34:04 +0200

    rebuild tester function

--- scraproxy.py
+++ scraproxy.py
@@ -59,51 +59,51 @@ def fileCreator(proxies):
 def cleaner(proxies, key):
     if key == 0:
         proxies = [proxy.replace(';', ':') for proxy in proxies]
+        proxies = [proxy.lower() for proxy in proxies]
         proxies = [proxy.replace('https:', '') for proxy in proxies]
         proxies = [proxy.replace('http:', '') for proxy in proxies]
-        proxies = [proxy.replace('Socks4:', 'socks4://') for proxy in proxies]
-        proxies = [proxy.replace('Socks5:', 'socks5://') for proxy in proxies]
+        proxies = [proxy.replace('socks4:', 'socks4://') for proxy in proxies]
+        proxies = [proxy.replace('socks5:', 'socks5://') for proxy in proxies]
     elif key == 1:
         proxies = [proxy.replace(':', ';') for proxy in proxies]
+        proxies = [proxy.lower() for proxy in proxies]
         proxies = [proxy.replace('socks4;//', 'socks4;') for proxy in proxies]
         proxies = [proxy.replace('socks5;//', 'socks5;') for proxy in proxies]
-        # proxies = [proxy.replace('', '') for proxy in proxies] #http & https
 
     return proxies
 
-def tester(proxies, ip):
+def tester(proxies, ip, ref_IPs):
     whitelist = []
 
-    for proxy in proxies:
-        try:
-            rep = requests.get("http://ifconfig.me/ip", {'http': proxy, 'https': proxy})
-            if rep.text == ip:
-                whitelist.append(proxy)
-            else:
-                proxies.remove(proxy)
-        except:
-            proxies.remove(proxy)
+    for proxy in range(0, len(proxies), 1):
+        rep = requests.get("http://ifconfig.me/ip", {'http': proxies[proxy], 'https': proxies[proxy]}, timeout=1)
+        if rep.text == ip:
+            whitelist.append(ref_IPs[proxy])
     fileCreator(cleaner(whitelist, 1))
 
 def launcher(nbr):
-    proxiAdrss = []
     ip = areyouconnected()
+    list_IPs = []
+    good_IP = []
+    count = 0
 
     if ip != 1:
         print("- step n°1 start    (scrapping)")
-        with open('list.csv', 'a') as file:
-            for elem in get_infoFrom("https://free-proxy-list.net/", 0, nbr):
-                proxiAdrss.append(elem)
-                file.write('%s\n' % elem)
-            for elem in get_infoFrom("https://www.socks-proxy.net/", 1, nbr):
-                proxiAdrss.append(elem)
-                file.write('%s\n' % elem)
-            # for page in next_page("https://hidemy.name/en/proxy-list/?start=", [1216,64,0]):
-            #     for elem in get_infoFrom(page, 2, nbr):
-            #         proxiAdrss.append(elem)
-            #         file.write('%s\n' % elem)
+        for elem in get_infoFrom("https://free-proxy-list.net/", 0, nbr):
+            list_IPs.append(elem)
+        for elem in get_infoFrom("https://www.socks-proxy.net/", 1, nbr):
+            list_IPs.append(elem)
+        # for page in next_page("https://hidemy.name/en/proxy-list/?start=", [1216,64,0]):
+        #     for elem in get_infoFrom(page, 2, nbr):
+        #         list_IPs.append(elem)
+        with open('list.csv', 'w') as fp:
+            for ipline in list_IPs:
+                fp.write('%s\n' % ipline)
+                good_IP.append(ipline)
         print("- step n°1 done\n- step n°2 start    (testing)")
-        tester(cleaner(proxiAdrss, 0), ip)
+        list_IPs = good_IP
+
+        tester(cleaner(good_IP, 0), ip, list_IPs)
         print("- step n°2 done")
     else:
         print("- end of execution of Scraproxy")
